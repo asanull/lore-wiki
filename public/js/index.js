1841784b8eb9d6d345547384f25c0fe25975baf9
asa-null <arthurhickman2@gmail.com>
2021-10-12 20:34:33 +0100

    tidy up

--- public/js/index.js
+++ public/js/index.js
@@ -2,35 +2,22 @@ const client = {
     username: document.getElementById('username').innerText,
     avatar: document.getElementById('avatar')
 }
-
 var img = new Image();
 var base64 = avatar.innerText
 img.src = base64
-
 var canvas=document.getElementById("canvas");
 var ctx=canvas.getContext("2d");
-
-function render(scale)
-{
+function render(scale) {
     canvas.width = scale
     canvas.height = scale
     ctx.drawImage(img,0,0,scale,scale)
 }
-
-function logResize()
-{
+function logResize() {
     if(window.innerWidth>1917)
-    {
-        render(84)
-    }
+    { render(84) }
     else if(window.innerWidth>1599)
-    {
-        render(74)
-    }
+    { render(74) }
     else
-    {
-        render(64)
-    }
+    { render(64) }
 }
-
 window.addEventListener('resize', logResize);
