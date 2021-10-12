96578a72736c2fc42a5c23b37358dc734426dfeb
asa-null <arthurhickman2@gmail.com>
2021-10-12 20:29:42 +0100

    better canvas scaling

--- public/js/index.js
+++ public/js/index.js
@@ -9,3 +9,28 @@ img.src = base64
 
 var canvas=document.getElementById("canvas");
 var ctx=canvas.getContext("2d");
+
+function render(scale)
+{
+    canvas.width = scale
+    canvas.height = scale
+    ctx.drawImage(img,0,0,scale,scale)
+}
+
+function logResize()
+{
+    if(window.innerWidth>1917)
+    {
+        render(84)
+    }
+    else if(window.innerWidth>1599)
+    {
+        render(74)
+    }
+    else
+    {
+        render(64)
+    }
+}
+
+window.addEventListener('resize', logResize);
